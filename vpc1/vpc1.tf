627d6090c6ff146c3b517d1ccf0b7db895f213dd
M&M <112832477+memor24@users.noreply.github.com>
2023-08-16 00:33:38 -0600

    vpc1 template with public subnet, etc

---
+++ vpc1/vpc1.tf
@@ -0,0 +1,59 @@
+terraform {
+  required_version = ">= 0.15.0"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_vpc" "vpc1" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id     = aws_vpc.vpc1.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+}
+
+resource "aws_security_group" "ecurity_group1" {
+  name        = "security-group1"
+  description = "security group for ec2 instance"
+  vpc_id      = aws_vpc.vpc1.id
+
+  // Inbound rule for SSH access
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  // Inbound rule for http access
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  // Outbound rule allowing all traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "instance1" {
+  ami           = "ami-0c55b159cbfafe1f0"  // AWS Linux free tier ami
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.subnet1.id
+  security_groups = [aws_security_group.security_group1.id]
+  key_name = "key-pair-1"
+
+  tags = {
+    Name = "Instance1"
+  }
+}
