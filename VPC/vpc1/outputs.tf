aae14d185c13ab59c8078099449a0efef714ee88
memor24linux <myemail@example.com>
2024-09-08 07:35:02 -0600

    tf-test update

--- VPC/vpc1/outputs.tf
+++ VPC/vpc1/outputs.tf
@@ -3,7 +3,7 @@ output "vpc_id" {
   value       = aws_vpc.vpc1.id
 }
 
-output "aws_internet_gateway_id" {
+output "aws_igw_id" {
   value = aws_internet_gateway.igw.id
   description = "The id of the internet gateway"
 }
