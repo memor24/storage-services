9d9e9b96e5765ae2afef50f860335d6ec2a7d26d
memor24linux <myemail@example.com>
2024-09-13 11:50:09 -0600

    updated ec2 tf files

--- EC2/stdn.tf
+++ EC2/stdn.tf
@@ -1,3 +1,6 @@
+
+#stnd.tf
+
 terraform {
   required_version = ">= 0.15.0"
   required_providers {
@@ -11,7 +14,7 @@ terraform {
 provider "aws" {
   region = "us-west-2"
 }
-
+ 
 resource "aws_instance" "instance1" {
   ami             = "ami-0c55b159cbfafe1f0" // sample ami for aws linux free tier
   instance_type   = t2.micro
@@ -19,17 +22,6 @@ resource "aws_instance" "instance1" {
   key_name        = "key_pair_1"
 
   tags = {
-    Name = "appserverec2"
+    Name = "ec2instance1"
   }
-
-
-user_data = <<-EOF
-            #!/bin/bash
-            sudo service apache2 start
-            EOF
 }
-
-
-# web server
-# cluster of web servers
-
