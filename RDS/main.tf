894115ed2ba521696346201d88b5370458b226ad
Mehdi <112832477+memor24@users.noreply.github.com>
2024-10-13 05:08:36 -0600

    Update main.tf

--- RDS/main.tf
+++ RDS/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.62"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
 
 module db {
   source = "./modules/db"
