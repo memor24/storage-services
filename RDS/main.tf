2447dd210a63e5f91ed1753cd09d148bf4e8c486
memor24linux <myemail@example.com>
2024-10-30 23:21:26 +0330

    rds tbu

--- RDS/main.tf
+++ RDS/main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_version = ">= 1.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.62"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-}
-
 module db {
   source = "./modules/db"
   param  = value #tbu
